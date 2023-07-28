252d999ade91dec8adde726077034365b3989250
Furki <74312970+fu-w@users.noreply.github.com>
2023-07-28 20:49:07 +0300

    Update examp.js

--- Commands/examp.js
+++ Commands/examp.js
@@ -1,5 +1,5 @@
 
-const { SlashCommandBuilder } = require("@discordjs/builders");
+let { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
     data: new SlashCommandBuilder()
